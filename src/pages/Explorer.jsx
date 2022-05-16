1d7670de8d8df3df365ebb1a1f2efc1248b6e899
drewb73 <bartondrew5@gmail.com>
2022-05-16 13:45:39 -0700

    swiper update

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
 import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+import Slider from '../components/Slider'
 
 function Explorer() {
   return (
@@ -10,7 +11,7 @@ function Explorer() {
           <p className='pageHeader'>Explore</p>
         </header>
         <main>
-          {/* Slide */}
+          <Slider />
 
           <p className='exploreCategoryHeading'>Categories</p>
           <div className='exploreCategories'>
